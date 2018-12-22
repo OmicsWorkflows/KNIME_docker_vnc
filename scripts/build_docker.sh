a3a4743b9aa51193b28e3448aefbd273ff93bc71
David <david.potesil@ceitec.muni.cz>
2018-12-22 23:47:35 +0100

    build_docker.sh corrected so it is ok with the current location

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,7 +3,7 @@
 #you can start it directly with 1 parameter: [VERSION]
 # it is placed in the main folder because of the build context of docker build - this way it can access files_to_copy/ folder
 
-dockerfile_location="./dockerfiles"
+dockerfile_location="./../dockerfiles"
 
 if (($# < 1 ));then
 echo "Which version of dockerfile should we use?"
