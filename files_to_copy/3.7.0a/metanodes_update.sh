d1ed35bbed99e71e710c3599d0a29dca5803709b
David <david.potesil@ceitec.muni.cz>
2019-01-12 00:17:57 +0100

    added sh files for the git repositories reset

---
+++ files_to_copy/3.7.0a/metanodes_update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# navigates to the KNIME_Metanodes repository, change the folder if needed
+cd ~/knime-workspace/gitfolders/KNIME_Metanodes/
+
+# resets the repository; change "master" branch to the appropriate one if needed
+git reset --hard origin/master
---
+++ files_to_copy/3.7.0a/workflows_update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# navigates to the KNIME_Workflows repository, change the folder if needed
+cd ~/knime-workspace/gitfolders/KNIME_Workflows/
+
+# resets the repository; change "master" branch to the appropriate one if needed
+git reset --hard origin/master
