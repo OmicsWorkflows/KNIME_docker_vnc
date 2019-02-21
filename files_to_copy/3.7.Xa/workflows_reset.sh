cfca93c40c69b1ee569393ef58e2379cd32c54b4
weiclav <weiclav@seznam.cz>
2019-02-21 18:12:36 +0100

    corrects git repositories reset

--- files_to_copy/3.7.Xa/workflows_reset.sh
+++ files_to_copy/3.7.Xa/workflows_reset.sh
@@ -4,6 +4,7 @@
 cd /home/knimeuser/knime-workspace/gitfolders/KNIME_Workflows
 
 # resets the repository; change "master" branch to the appropriate one if needed
+git fetch origin
 git reset --hard origin/master
 # removes also not tracked folders and files
 git clean -f -d
