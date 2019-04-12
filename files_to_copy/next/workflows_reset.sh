e1978c6fc60b68441a9bc260b2714c82884c21f8
weiclav <weiclav@seznam.cz>
2019-04-12 09:34:05 +0200

    files for the next version created

---
+++ files_to_copy/next/workflows_reset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# shows the notification about the initiated task
+notify-send -i software-update-available "Workflows templates repository" \
+"<b>Repository reset started!</b> \
+\rPlease wait till it is finished..."
+
+# navigates to the KNIME_Workflows repository, change the folder if needed
+cd /home/knimeuser/knime-workspace/gitfolders/KNIME_Workflows
+
+# resets the repository; change "master" branch to the appropriate one if needed
+git fetch origin
+git reset --hard origin/master
+# removes also not tracked folders and files
+git clean -f -d
+
+# shows the notification about the finished task
+notify-send -i process-completed "Workflows templates repository" \
+"<b>Repository reset completed!</b> \
+\rYou can start to use them in KNIME! \
+\rWorkspace refresh may be needed..."
