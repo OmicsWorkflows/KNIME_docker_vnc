ba341a9186f817b771239ecde2937b3518fc335a
weiclav <weiclav@seznam.cz>
2020-02-25 10:56:54 +0100

    github repositories reset scripts checks the folders presence

--- files_to_copy/4.1.1a/workflows_reset.sh
+++ files_to_copy/4.1.1a/workflows_reset.sh
@@ -5,8 +5,21 @@ notify-send -i software-update-available "Workflows templates repository" \
 "<b>Repository reset started!</b> \
 \rPlease wait till it is finished..."
 
-# navigates to the KNIME_Workflows repository, change the folder if needed
-cd /home/knimeuser/knime-workspace/gitfolders/KNIME_workflows
+# repository directory
+directory=/home/knimeuser/knime-workspace/gitfolders/KNIME_workflows
+
+# checks whether the repository folder exists and shows notification if not
+if [ ! -d "$directory" ]; then
+  # shows the notification about the missing repository folder
+  notify-send -u critical -i dialog-error "Metanodes templates repository" \
+  "<b>Repository folder is not present!</b> \
+  \rExpected directory: \
+  \r$directory"
+  exit 1
+fi
+
+# navigates to the KNIME_Workflows repository
+cd $directory
 
 # resets the repository; change "master" branch to the appropriate one if needed
 git fetch origin
@@ -15,7 +28,8 @@ git reset --hard origin/master
 git clean -f -d
 
 # shows the notification about the finished task
-notify-send -i process-completed "Workflows templates repository" \
+notify-send -u critical -i process-completed "Workflows templates repository" \
 "<b>Repository reset completed!</b> \
 \rYou can start to use them in KNIME! \
-\rWorkspace refresh may be needed..."
+\rWorkspace refresh in KNIME may be needed! \
+\rClick here to hide the notification"
