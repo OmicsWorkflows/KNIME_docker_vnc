5a13daf2ebe3a4bc4547215f1ce4a9f2658b2aca
weiclav <weiclav@seznam.cz>
2020-02-25 08:50:23 +0100

    /dev/shm folder decreased to 1g

--- scripts/start_container.bat
+++ scripts/start_container.bat
@@ -77,7 +77,7 @@ if not exist !workspace_path! (
     exit
 )
 
-docker run -it --shm-size=4g --name knime%port% -p %port%:5901 -v %workspace_path%:%volume_mount_point% -e CONTAINER_TIMEZONE=%timezone% -e TZ=%timezone% --rm %image_name%
+docker run -it --shm-size=1g --name knime%port% -p %port%:5901 -v %workspace_path%:%volume_mount_point% -e CONTAINER_TIMEZONE=%timezone% -e TZ=%timezone% --rm %image_name%
 
 if errorlevel 1 (
    echo Error level returned is %errorlevel%
