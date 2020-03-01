b1431f4ba392790eeebe5d0d537673024025c3ec
weiclav <weiclav@seznam.cz>
2020-03-01 01:54:01 +0100

    initial adjustment of the doker file so it installs python3 only at first and different python environment afterwards using pipenv

---
+++ files_to_copy/4.1.1b/home_folder/.local/share/virtualenvs/3.7.2a/3.7.2a_py36.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+. /home/ubuntu/.local/share/virtualenvs/3.7.2a/py36/bin/activate
+python "$@" 1>&1 2>&2
---
+++ files_to_copy/4.1.1b/home_folder/.local/share/virtualenvs/4.1.1b/4.1.1b_py37.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+. /home/ubuntu/.local/share/virtualenvs/4.1.1b/py37/bin/activate
+python "$@" 1>&1 2>&2
