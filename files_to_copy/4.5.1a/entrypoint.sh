41cb9c88b178a946f3460b70385f65ac6bc977ce
weiclav <weiclav@seznam.cz>
2022-02-11 14:39:11 +0100

    4.5.1a version initial commit

---
+++ files_to_copy/4.5.1a/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -n $VNCPASSWORD | vncpasswd -f > ~/.vnc/passwd
+chmod go-rwx ~/.vnc/passwd
+
+export VNCPASSWORD=""
+
+if [ ! -v "$CONTAINER_TIMEZONE" ]; then
+ echo ${CONTAINER_TIMEZONE} >/etc/timezone
+# problem with permissions for this symlink if running container under knimeuser
+# ln -sf /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime
+ echo "container timezone set to ${CONTAINER_TIMEZONE}"
+fi
+
+# remove display lock - if locked, vnc cannot initiate
+# happens eg. if updating container to new image (the lock from first vnc will be saved)
+rm -f /tmp/.X1-lock
+rm -f /tmp/.X11-unix/*
+vncserver :1 -geometry 1680x1050 -depth 24 && tail -F $HOME/.vnc/*.log
