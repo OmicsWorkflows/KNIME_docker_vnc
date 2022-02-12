0e86a7e8ad474533dac5e377288c3831a827b10e
weiclav <weiclav@seznam.cz>
2022-02-12 17:49:33 +0100

    dockerfile build script adjusted to handle different dockeriles like knime and environments

--- build_docker.sh
+++ build_docker.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#you can start it directly with 1 parameter: [VERSION]
-# it is placed in the main folder because of the build context of docker build - this way it can access files_to_copy/ folder
+# the script is placed in the main folder because of the build context of docker build - this way it can access files_to_copy/ folder
 
 dockerfile_location="./dockerfiles"
 
-if (($# < 2 ));then
 echo "Which dockerfile should be used?"
 read dockerfile
 echo "Which version of dockerfile should be used?"
 read version
-else
-dockerfile=$1
-version=$2
-fi
 
 docker build -t $dockerfile:$version -f ${dockerfile_location}/Dockerfile_${dockerfile}_${version} .
