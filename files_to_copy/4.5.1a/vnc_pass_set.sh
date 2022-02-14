073ef8d80dd475be993a3a82e1f19da565fcad2c
weiclav <weiclav@seznam.cz>
2022-02-14 23:24:33 +0100

    adds shortcut for the VNC pass set

---
+++ files_to_copy/4.5.1a/vnc_pass_set.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while true; do
+  ENTRY=`zenity --password --title="VNC password"`
+  len=`expr length "$ENTRY"`
+  case $? in
+     0)
+	if ((len < 6))
+	then
+	  zenity --error --width=400 --text="Password have to be at least 6 characters long!\\n\\nPlease try again." --title="Warning!"
+	  continue
+	fi
+	printf "`echo $ENTRY`\n`echo $ENTRY`\n\n" | vncpasswd
+	zenity --info --text="VNC password changed!" --width=200
+	break
+	;;
+     1)
+        zenity --info --text="Password entry stopped." --title="Entry stopped"  --width=200
+        break
+        ;;
+    -1)
+        zenity --error --text="An unexpected error has occurred." --title="Error!"  --width=200
+        break
+  esac
+done
