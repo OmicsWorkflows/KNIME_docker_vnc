fc16b19138466904f0536559bf99d545099a1f04
weiclav <weiclav@seznam.cz>
2022-02-18 01:15:21 +0100

    last branch selection added

--- files_to_copy/4.5.1a/metanodes_reset_branch.sh
+++ files_to_copy/4.5.1a/metanodes_reset_branch.sh
@@ -2,6 +2,8 @@
 
 # repository directory
 directory=/home/knimeuser/knime-workspace/gitfolders/KNIME_metanodes
+# file to hold the last used branch
+last_branch_metanode_file=~/.last_branch_metanode
 
 # checks whether the repository folder exists and shows notification if not
 if [ ! -d "$directory" ]; then
@@ -16,28 +18,41 @@ fi
 # navigates to the KNIME_Metanodes repository
 cd "$directory"
 
+# gets the list of all remote branches
 MULTILINE=`git branch -r | grep -v ‘remotes’ \
   -1`
 
-ENTRY=`zenity --list --column="GitHub branches - KNIME metanode templates" "master" ${MULTILINE} --height=500 --width=500 --title="GitHub branch selection"`
+# checks the presence of file holding the last used branch and if it is not present, sets master branch as the last used
+if test -f $last_branch_metanode_file; then
+  read LAST_BRANCH < $last_branch_metanode_file
+else
+  LAST_BRANCH="master"
+fi
 
-case $? in
-   0)
-      if (($ENTRY == "master"))
-      then
-        branch="origin/master"
-      else
-        branch=$ENTRY
-      fi
-      ;;
-   1)
-      exit 1
-      ;;
-  -1)
-      zenity --error --text="An unexpected error has occurred." --title="Error!"  --width=200
-      exit 1
-esac
+# while loop to ask again in case of empty branch name selected
+while true; do
+ENTRY=`zenity --list --column="GitHub branches - KNIME metanode templates" "master" ${LAST_BRANCH} "" ${MULTILINE} --height=500 --width=500 --title="GitHub branch selection"`
 
+  case $? in
+     0)
+        if [ $ENTRY == "master" ]; then
+          branch="origin/master"
+        elif [ -z "${ENTRY}" ]; then
+          zenity --warning --text="Empty line selected. Please, select again." --title="Empty line!"  --width=200
+          continue
+        else
+          branch=$ENTRY
+        fi
+        break
+        ;;
+     1)
+     	break
+        ;;
+    -1)
+        zenity --error --text="An unexpected error has occurred." --title="Error!"  --width=200
+        break
+  esac
+done
 echo "branch set to $branch"
 
 # shows the notification about the initiated task
@@ -47,10 +62,12 @@ notify-send -i software-update-available "Metanodes templates repository" \
 
 # resets the repository; change "master" branch to the appropriate one if needed
 git fetch origin
-#git reset --hard origin/master
 git reset --hard $branch
 # removes also not tracked folders and files
 git clean -f -d
+
+# writes down small file that will hold the last used branch name
+echo $branch > $last_branch_metanode_file
 
 # shows the notification about the finished task
 notify-send -u critical -i process-completed "Metanodes templates repository" \
