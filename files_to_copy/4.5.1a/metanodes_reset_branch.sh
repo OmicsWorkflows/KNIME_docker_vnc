a8b1699618061a5c6f4def58c98f61aca404d8c4
weiclav <weiclav@seznam.cz>
2022-02-18 09:17:36 +0100

    branch presence check added

--- files_to_copy/4.5.1a/metanodes_reset_branch.sh
+++ files_to_copy/4.5.1a/metanodes_reset_branch.sh
@@ -31,26 +31,40 @@ fi
 
 # while loop to ask again in case of empty branch name selected
 while true; do
-ENTRY=`zenity --list --column="GitHub branches - KNIME metanode templates" "master" ${LAST_BRANCH} "" ${MULTILINE} --height=500 --width=500 --title="GitHub branch selection"`
-
+  ENTRY=`zenity --list --column="GitHub branches - KNIME metanode templates" "master" ${LAST_BRANCH} "" ${MULTILINE} --height=300 --width=500 --title="GitHub branch selection"`
   case $? in
      0)
         if [ $ENTRY == "master" ]; then
           branch="origin/master"
         elif [ -z "${ENTRY}" ]; then
-          zenity --warning --text="Empty line selected. Please, select again." --title="Empty line!"  --width=200
+          zenity --warning --text="No branch name specified. Please, select again." --title="No branch name"  --width=200
           continue
         else
           branch=$ENTRY
         fi
+        if [ -n "`git show-ref --verify refs/remotes/"$branch"`" ]; then
+          #echo 'branch exists!'
+          # resets the repository; change "master" branch to the appropriate one if needed
+          #git fetch origin
+          #git reset --hard $branch
+          # removes also not tracked folders and files
+          #git clean -f -d
+          # writes down small file that will hold the last used branch name
+          #echo $branch > $last_branch_metanode_file
+          break
+        else
+          echo 'branch does not exists!'
+          zenity --error --text="Selected branch does not exists!\n\nIt might have been deleted recently.\nPlease verify and select again.\n\nChecked branch name:\n"$branch"" --title="Branch does not exists!"  --width=300
+          continue
+        fi
         break
         ;;
      1)
-     	break
+     	exit 1
         ;;
     -1)
         zenity --error --text="An unexpected error has occurred." --title="Error!"  --width=200
-        break
+        exit 1
   esac
 done
 echo "branch set to $branch"
@@ -65,9 +79,7 @@ git fetch origin
 git reset --hard $branch
 # removes also not tracked folders and files
 git clean -f -d
-
 # writes down small file that will hold the last used branch name
-echo $branch > $last_branch_metanode_file
 
 # shows the notification about the finished task
 notify-send -u critical -i process-completed "Metanodes templates repository" \
