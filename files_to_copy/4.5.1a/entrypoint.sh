2914d36d0f9e4d50a726adc33ed14b0feab14bcf
weiclav <weiclav@seznam.cz>
2022-02-24 17:16:41 +0100

    set the timezone also via /etc/localtime so it is more compatible with the systems where root or user with sudo rights is not allowed

--- files_to_copy/4.5.1a/entrypoint.sh
+++ files_to_copy/4.5.1a/entrypoint.sh
@@ -6,12 +6,14 @@ chmod go-rwx ~/.vnc/passwd
 export VNCPASSWORD=""
 
 if [ ! -v "$CONTAINER_TIMEZONE" ]; then
- echo ${CONTAINER_TIMEZONE} >/etc/timezone
+ echo ${CONTAINER_TIMEZONE} > /etc/timezone
 # problem with permissions for this symlink if running container under knimeuser
 # ln -sf /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime
  echo "container timezone set to ${CONTAINER_TIMEZONE}"
  # needs to be set so tidyverse works as expected, systemd related error observed otherwise
  echo "TZ='${CONTAINER_TIMEZONE}'; export TZ" >> ~/.bashrc
+ # this settings helps to set the timezone also in systems where root/user with sudo rights is not allowed
+ cat /usr/share/zoneinfo/${CONTAINER_TIMEZONE} > /etc/localtime
 fi
 
 # remove display lock - if locked, vnc cannot initiate
