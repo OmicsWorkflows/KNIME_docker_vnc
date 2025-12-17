dc1071ef4eb4084bcc15c8b5a7cc75154f6c6ddb
Heriette <la.heriette@gmail.com>
2025-12-17 14:46:59 +0100

    removed no longer used files

--- files_to_copy/5.8.0a/set_metadata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-f="$HOME/Desktop/knime.desktop"
-
-if [ -f "$f" ]; then
-    checksum="$(sha256sum "$f" | awk '{print $1}')"
-    gio set -t string "$f" metadata::xfce-exe-checksum "$checksum"
-fi
