df77b1b37ca2cf8f03034c90f9ec73db8b959e75
Heriette <la.heriette@gmail.com>
2026-05-20 12:27:57 +0200

    added scripts with workflow unzipping

---
+++ files_to_copy/5.9.0a-pre3/vnc_pass_set.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while true; do
+  ENTRY=`zenity --password --title="VNC password"`
+  len=`expr length "$ENTRY"`
+  case $? in
+     0)
+	if ((len < 6))
+	then
+	  zenity --error --width=400 --text="Password has to be at least 6 characters long!\\n\\nPlease try again." --title="Warning!"
+	  continue
+	fi
+	printf "`echo $ENTRY`\n`echo $ENTRY`\n\n" | vncpasswd
+	zenity --info --text="VNC password changed!" --width=200
+	break
+	;;
+     1)
+        zenity --info --text="Password entry canceled, exitting." --title="Canceled"  --width=200
+        exit 1
+        ;;
+    -1)
+        zenity --error --text="An unexpected error has occurred." --title="Error!"  --width=200
+        exit 1
+  esac
+done
