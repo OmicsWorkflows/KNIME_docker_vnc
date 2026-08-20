1126dae7758a087d35268b210d143118a83cab54
Heriette <la.heriette@gmail.com>
2026-08-20 12:21:22 +0200

    removed previous working versions

--- files_to_copy/5.9.0a-pre4/knime_with_workspace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# start KNIME with dialog to select the workspace folder
-#$knime_folder/knime
-
-# OR enable this to start KNIME without to select the workspace folder selecting the default one automatically
-$knime_folder/knime -data "${knime_workspace_folder}"
